cf34906ffa9a51eefc826b97e0225ab8e0273ba1
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2018-10-21 17:50:46 -0300

    Add application core

---
+++ src/main/kotlin/com/github/rodolphocouto/core/application/HotDealService.kt
@@ -0,0 +1,61 @@
+package com.github.rodolphocouto.core.application
+
+import com.github.rodolphocouto.core.domain.HotDeal
+import com.github.rodolphocouto.core.domain.HotDealAlreadyExistsException
+import com.github.rodolphocouto.core.domain.HotDealId
+import com.github.rodolphocouto.core.domain.HotDealNotFoundException
+import com.github.rodolphocouto.core.domain.HotDealRepository
+import com.github.rodolphocouto.core.domain.Merchant
+import org.springframework.stereotype.Service
+import reactor.core.publisher.toMono
+
+@Service
+class HotDealService(private val repository: HotDealRepository) {
+
+    fun findHotDeals() = repository.findAll().map { it.toQuery() }
+
+    fun findHotDealStream() = repository.findStream().map { it.toQuery() }
+
+    fun findHotDealById(hotDealId: HotDealId) =
+        repository.findById(hotDealId).map { it.toQuery() }.switchIfEmpty(HotDealNotFoundException().toMono())
+
+    fun createHotDeal(command: HotDealCreationCommand) =
+        repository.findByMerchantNameAndCategory(command.merchant.name, command.merchant.category)
+            .flatMap { HotDealAlreadyExistsException().toMono<HotDeal>() }
+            .switchIfEmpty(
+                repository.create(
+                    HotDeal(
+                        merchant = Merchant(
+                            name = command.merchant.name,
+                            category = command.merchant.category,
+                            address = command.merchant.address,
+                            rating = command.merchant.rating
+                        ),
+                        cashback = command.cashback,
+                        endTime = command.endTime
+                    )
+                )
+            )
+
+    fun updateHotDeal(command: HotDealUpdateCommand) =
+        repository.findById(command.hotDealId)
+            .map {
+                it.copy(
+                    merchant = Merchant(
+                        name = command.merchant?.name ?: it.merchant.name,
+                        category = command.merchant?.category ?: it.merchant.category,
+                        address = command.merchant?.address ?: it.merchant.address,
+                        rating = command.merchant?.rating ?: it.merchant.rating
+                    ),
+                    cashback = command.cashback ?: it.cashback,
+                    endTime = command.endTime ?: it.endTime
+                )
+            }
+            .flatMap { repository.update(it) }
+            .switchIfEmpty(HotDealNotFoundException().toMono())
+
+    fun removeHotDeal(command: HotDealRemovalCommand) =
+        repository.findById(command.hotDealId)
+            .map { repository.remove(it) }
+            .switchIfEmpty(HotDealNotFoundException().toMono())
+}
---
+++ src/main/kotlin/com/github/rodolphocouto/core/domain/HotDeal.kt
@@ -0,0 +1,51 @@
+package com.github.rodolphocouto.core.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import org.valiktor.functions.hasDecimalDigits
+import org.valiktor.functions.hasSize
+import org.valiktor.functions.isBetween
+import org.valiktor.functions.isGreaterThan
+import org.valiktor.functions.isNotBlank
+import org.valiktor.functions.validate
+import org.valiktor.validate
+import java.time.LocalDateTime
+import java.util.UUID
+
+typealias HotDealId = UUID
+
+class HotDealNotFoundException : Exception()
+class HotDealAlreadyExistsException : Exception()
+
+@Document
+data class HotDeal(
+    @Id val hotDealId: HotDealId,
+    val merchant: Merchant,
+    val cashback: Int,
+    val startTime: LocalDateTime,
+    val endTime: LocalDateTime
+) {
+    constructor(merchant: Merchant, cashback: Int, endTime: LocalDateTime)
+        : this(HotDealId.randomUUID(), merchant, cashback, LocalDateTime.now(), endTime)
+
+    init {
+        validate(this) {
+            validate(HotDeal::cashback).isBetween(1, 100)
+            validate(HotDeal::endTime).isGreaterThan(this@HotDeal.startTime)
+            validate(HotDeal::merchant).validate {
+                validate(Merchant::name).isNotBlank().hasSize(max = 50)
+                validate(Merchant::address).isNotBlank().hasSize(max = 100)
+                validate(Merchant::rating).isBetween(0.0, 5.0).hasDecimalDigits(max = 2)
+            }
+        }
+    }
+}
+
+enum class MerchantCategory { RESTAURANT, FUEL, HEALTH }
+
+data class Merchant(
+    val name: String,
+    val category: MerchantCategory,
+    val address: String,
+    val rating: Double
+)
