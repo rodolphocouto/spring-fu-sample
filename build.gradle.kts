3a9711933812a255d036c338bb5dc5f143d0702a
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2018-11-02 14:33:42 -0300

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import org.gradle.kotlin.dsl.version
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.0-rc-190"
+    id("org.jetbrains.kotlin.jvm") version "1.3.0"
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
-    id("org.springframework.boot") version "2.1.0.RC1"
-    id("org.jmailen.kotlinter") version "1.16.0"
-    id("com.adarshr.test-logger") version "1.3.1"
+    id("org.springframework.boot") version "2.1.0.RELEASE"
+    id("org.jmailen.kotlinter") version "1.20.1"
+    id("com.adarshr.test-logger") version "1.5.0"
 }
 
 dependencies {
@@ -18,19 +18,17 @@ dependencies {
     implementation("org.valiktor:valiktor-javatime:0.3.1")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.1.8")
-    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.0.0-RC3")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.1.10")
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.0.0")
     testImplementation("org.springframework:spring-test")
     testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 }
 
 repositories {
-    mavenLocal()
     mavenCentral()
     maven("https://repo.spring.io/milestone")
     maven("https://repo.spring.io/snapshot")
-    maven("http://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 tasks.withType<KotlinCompile> {
